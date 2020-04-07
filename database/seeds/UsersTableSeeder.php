f4781b49729bfde9f8be57efba0bb662922b06c5
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-04-07 12:39:34 +0500

    UsersTableSeeder.php update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,9 +14,27 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Daler Nazarov',
-            'email' => 'd@n.tj',
-            'password' => Hash::make('28021992')
+            'name' => 'Sultonazar',
+            'last_name' => 'Mamadazizov',
+            'patronymic' => 'Shodievich',
+            'email' => 'sultonazar.mamadazizov@mail.ru',
+            'password' => Hash::make('Dhtvtyysq92')
+        ]);
+
+        User::create([
+            'name' => 'Nekruz',
+            'last_name' => 'Dastambuev',
+            'patronymic' => null,
+            'email' => 'test@test.com',
+            'password' => Hash::make('YtrRekWd23')
+        ]);
+
+        User::create([
+            'name' => 'Далер',
+            'last_name' => 'Назаров',
+            'patronymic' => 'Мехрубонович',
+            'email' => 'test2@test.com',
+            'password' => Hash::make('YtrRekWd23')
         ]);
     }
 }
